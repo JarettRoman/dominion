9cf418787c672531ba1193c337039d292a2a858c
Jarett <afrocious@gmail.com>
2016-01-21 08:52:29 -0500

    this code never hurt anyone!

--- app.py
+++ app.py
@@ -86,6 +86,6 @@ else:
 
         return render_template('main.html', form=form)
 
-    # if __name__ == '__main__':
-    #     app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True)
 
