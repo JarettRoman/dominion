2e6e0b2c18b41172482e6bc60d371d37e874c7f4
Jarett <afrocious@gmail.com>
2016-01-21 15:36:37 -0500

    refactored DB connection functionality

--- app.py
+++ app.py
@@ -1,92 +1,72 @@
 from flask import Flask, render_template, request
-import MySQLdb
-import MySQLdb.cursors
-from instance import db_config
 from wtforms import *
 from pydash import _
 import utils
 import not_statement
 import re
+import db
 
 app = Flask(__name__)
 
-try:
-    cnx = MySQLdb.connect(
-        host = db_config.config['host'],
-        user = db_config.config['user'],
-        passwd = db_config.config['password'],
-        db = db_config.config['database'],
-        cursorclass = MySQLdb.cursors.DictCursor
-    )
+@app.route("/", methods=['GET', 'POST'])
+def main():
 
-except MySQLdb.Error as err:
-    print err
+    #WTForms validators
+    def set_checker(form, field):
+        if len(request.values) > 2:
+            return True
+        raise ValidationError('Please select at least one set.')
 
-else:
+    def blacklist_checker(form, field):
+        if re.match(".*[,;]$", request.values['blacklist']):
+            raise ValidationError('Please don\'t have trailing punctuation in the blacklist box.')
 
-    @app.route("/", methods=['GET', 'POST'])
-    def main():
+    class RandomizerForm(Form):
+        base = BooleanField('Base')
+        intrigue = BooleanField('Intrigue')
+        seaside = BooleanField('Seaside')
+        alchemy = BooleanField('Alchemy')
+        prosperity = BooleanField('Prosperity')
+        cornucopia = BooleanField('Cornucopia')
+        hinterlands = BooleanField('Hinterlands')
+        darkages = BooleanField('Dark Ages')
+        guilds = BooleanField('Guilds')
+        adventures = BooleanField('Adventures')
+        blacklist = TextAreaField('Blacklisted cards', [blacklist_checker])
 
-        #WTForms validators
-        def set_checker(form, field):
-            if len(request.values) > 2:
-                return True
-            raise ValidationError('Please select at least one set.')
+        randomize_button = SubmitField('Get cards!', [set_checker])
 
-        def blacklist_checker(form, field):
-            if re.match(".*[,;]$", request.values['blacklist']):
-                raise ValidationError('Please don\'t have trailing punctuation in the blacklist box.')
+    form = RandomizerForm(request.values)
 
-        class RandomizerForm(Form):
-            base = BooleanField('Base')
-            intrigue = BooleanField('Intrigue')
-            seaside = BooleanField('Seaside')
-            alchemy = BooleanField('Alchemy')
-            prosperity = BooleanField('Prosperity')
-            cornucopia = BooleanField('Cornucopia')
-            hinterlands = BooleanField('Hinterlands')
-            darkages = BooleanField('Dark Ages')
-            guilds = BooleanField('Guilds')
-            adventures = BooleanField('Adventures')
-            blacklist = TextAreaField('Blacklisted cards', [blacklist_checker])
+    if ('randomize_button' in request.values) and form.validate():
 
-            randomize_button = SubmitField('Get cards!', [set_checker])
+        conn = db.DB()
 
-        form = RandomizerForm(request.values)
+        sets = []
+        for set in _.keys(request.values):
+            if set == 'darkages':
+                sets.append("\'Dark Ages\'")
+            elif set != 'randomize_button' and set != 'blacklist':
+                sets.append("\'" + set + "\'")
 
-        if ('randomize_button' in request.values) and form.validate():
+        where_string = ', '.join(sets)
 
-            cursor = cnx.cursor()
+        query = """CREATE OR REPLACE VIEW picked_cards as
+                SELECT * FROM cards
+                WHERE CardSet IN ({}) AND ({}) ORDER BY RAND() LIMIT 10""".format(where_string,
+                                                                                  not_statement.blacklist(request.values['blacklist']))
 
-            sets = []
-            for set in _.keys(request.values):
-                if set == 'darkages':
-                    sets.append("\'Dark Ages\'")
-                elif set != 'randomize_button' and set != 'blacklist':
-                    sets.append("\'" + set + "\'")
+        conn.execute(query)
 
-            where_string = ', '.join(sets)
+        query = "SELECT * FROM picked_cards ORDER BY CardSet, Cost"
 
-            query = """CREATE OR REPLACE VIEW picked_cards as
-                    SELECT * FROM cards
-                    WHERE CardSet IN ({}) AND ({}) ORDER BY RAND() LIMIT 10""".format(where_string,
-                                                                                      not_statement.blacklist(request.values['blacklist']))
+        content = conn.execute(query).fetchall()
 
-            cursor.execute(query)
+        img_links = utils.cardImgLinker(content)
 
-            query = "SELECT * FROM picked_cards ORDER BY CardSet, Cost"
+        return render_template('main.html', form=form, links=img_links)
 
-            cursor.execute(query)
+    return render_template('main.html', form=form)
 
-            content = cursor.fetchall()
-
-            cursor.close()
-
-            img_links = utils.cardImgLinker(content)
-
-            return render_template('main.html', form=form, links=img_links)
-
-        return render_template('main.html', form=form)
-
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,24 @@
+import MySQLdb
+from instance import db_config
+
+class DB:
+  conn = None
+
+  def connect(self):
+    self.conn = MySQLdb.connect(
+        host = db_config.config['host'],
+        user = db_config.config['user'],
+        passwd = db_config.config['password'],
+        db = db_config.config['database'],
+        cursorclass = MySQLdb.cursors.DictCursor
+    )
+
+  def execute(self, sql):
+    try:
+      cursor = self.conn.cursor()
+      cursor.execute(sql)
+    except (AttributeError, MySQLdb.OperationalError):
+      self.connect()
+      cursor = self.conn.cursor()
+      cursor.execute(sql)
+    return cursor
